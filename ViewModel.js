4695a4144325086e9715895a82b02113baf6c1f3
Yermalovich Sviataslau <sviatosla.ermolovich@gmail.com>
2025-04-22 10:00:17 +0300

    feat: added viewModel file connected to html and added script to js file

---
+++ ViewModel.js
@@ -0,0 +1,11 @@
+class ViewModel {
+	constructor() {
+		console.log('initialize ViewModel class');
+		this.controller = new Controller();
+	}
+	
+	init() {
+	// Method which initialaize empty state UI
+	} 
+}
+
