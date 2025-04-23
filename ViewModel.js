b600b12ec69c3d79a29cd6d25684b40a3a0c597f
Yermalovich Sviataslau <sviatosla.ermolovich@gmail.com>
2025-04-23 17:29:19 +0300

    feat: created a file and initialized the controller class in it and exported it to the ViewModel file

--- ViewModel.js
+++ ViewModel.js
@@ -1,7 +1,9 @@
+import Controller from "./Controller.js";
+
 export default class ViewModel {
 	constructor() {
 		console.log('initialize ViewModel class');
-		//this.controller = new Controller();
+		this.controller = new Controller();
 	}
 	
 	init() {
