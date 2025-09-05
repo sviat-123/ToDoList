8ce1ea74c02ec8f1e08ac67b666ffb87b696dc53
Yermalovich Sviataslau <sviatosla.ermolovich@gmail.com>
2025-09-05 13:59:28 +0300

    feat: method to display task list

--- ViewModel.js
+++ ViewModel.js
@@ -8,6 +8,7 @@ export default class ViewModel {
 		//initializing todolist
 		this.main = document.createElement('div');
 		this.toDoList = document.createElement('div');
+		this.toDoListTitle = document.createElement('h1');
 		this.modalOpenButtonAddTask = document.createElement('button');
 		this.modalOpenButtonText = document.createElement('span');
 		this.modalOpenButtonIcon = document.createElement('img');
@@ -27,6 +28,15 @@ export default class ViewModel {
 		this.modalCancelButton = document.createElement('button');
 		this.modalSubmitButton = document.createElement('button');
 
+		//initializing tasks
+		this.taskList = document.createElement('ul');
+		this.task = document.createElement('li');
+		this.taskCheckBox = document.createElement('input');
+		this.taskTittle = document.createElement('p');
+		this.taskSubtitle = document.createElement('p');
+		this.taskEditButton = document.createElement('button');
+		this.taskDeleteButton = document.createElement('button');
+
 		//state of one window
 		this.isModalOpen = false;
 	}
@@ -40,77 +50,93 @@ export default class ViewModel {
 		this.toDoList.id = 'toDolist';
 		this.main.prepend(this.toDoList);
 
-		this.modalOpenButtonAddTask.classList = 'button-open-modal';
-		this.modalOpenButtonAddTask.id = 'button-open-modal';
-		this.toDoList.prepend(this.modalOpenButtonAddTask);
+		this.toDoListTitle.classList.add('to-Do-List__title');
+		this.toDoListTitle.id = 'toDolistTitle';
+		this.toDoListTitle.textContent = 'ToDo List';
+		this.toDoList.prepend(this.toDoListTitle);
 
-		this.modalOpenButtonText.classList = 'button-open-modal__text';
+		this.modalOpenButtonAddTask.classList.add('button-open-modal');
+		this.modalOpenButtonAddTask.id = 'button-open-modal';
+		this.toDoListTitle.after(this.modalOpenButtonAddTask);
+
+		this.modalOpenButtonText.classList.add('button-open-modal__text');
 		this.modalOpenButtonText.id = 'button-open-modal__text';
 		this.modalOpenButtonText.textContent = 'Add Todo';
 		this.modalOpenButtonAddTask.prepend(this.modalOpenButtonText);
 
-		this.modalOpenButtonIcon.classList = 'button-open-modal__icon';
+		this.modalOpenButtonIcon.classList.add('button-open-modal__icon');
 		this.modalOpenButtonIcon.id = 'button-open-modal__icon';
 		this.modalOpenButtonIcon.src = './icon/plus_icon.svg';
 		this.modalOpenButtonAddTask.prepend(this.modalOpenButtonIcon);
+
+		this.taskList.classList.add('task__list');
+		this.taskList.id = 'taskList';
+		this.modalOpenButtonAddTask.after(this.taskList);
 	}
 
 	displayModalWindow(){
-		this.modal.classList = 'modal';
+		this.modal.classList.add('modal');
 		this.modal.id = 'taskModal';
 		this.main.after(this.modal);
 
-		this.modalHeader.classList = 'block modal__header';
+		this.modalHeader.classList.add('modal__header');
+		this.modalHeader.classList.add('block');
 		this.modalHeader.id = 'taskModal__header';
 		this.modal.append(this.modalHeader)
 
-		this.modalTitle.classList = 'modal__title';
+		this.modalTitle.classList.add('modal__title');
 		this.modalTitle.id = 'taskModal__title';
 		this.modalTitle.textContent = 'Add Todo';
 		this.modalHeader.append(this.modalTitle);
 
-		this.modalCloseButton.classList = 'modal__button modal__button-close';
+		this.modalCloseButton.classList.add('modal__button-close');
+		this.modalCloseButton.classList.add('modal__button');
 		this.modalCloseButton.id = 'taskModal__closeButton';
 		this.modalHeader.append(this.modalCloseButton);
 
-		this.modalBody.classList = 'block modal__body';
+		this.modalBody.classList.add('modal__body');
+		this.modalBody.classList.add('block');
 		this.modalBody.id = 'taskModal__body';
 		this.modal.append(this.modalBody);
 
-		this.modalSubtitle_1.classList = 'modal__subtitle';
+		this.modalSubtitle_1.classList.add('modal__subtitle');
 		this.modalSubtitle_1.id = 'taskModal__subtitle_1';
 		this.modalSubtitle_1.textContent = 'Title';
 		this.modalBody.append(this.modalSubtitle_1);
 
-		this.modalInput.classList = 'modal__input';
+		this.modalInput.classList.add('modal__input');
 		this.modalInput.id = 'taskModal__titleInput';
 		this.modalInput.placeholder = 'What needs to be done?';
 		this.modalBody.append(this.modalInput);
 
-		this.modalDescripton.classList = 'block modal__description';
+		this.modalDescripton.classList.add('modal__description');
+		this.modalDescripton.classList.add('block');
 		this.modalDescripton.id = 'taskModal__description';
 		this.modal.append(this.modalDescripton);
 
-		this.modalSubtitle_2.classList = 'modal__subtitle';
+		this.modalSubtitle_2.classList.add('modal__subtitle');
 		this.modalSubtitle_2.id = 'taskModal__subtitle_2';
 		this.modalSubtitle_2.textContent = 'Description (optional)'
 		this.modalDescripton.append(this.modalSubtitle_2);
 
-		this.modalTextArea.classList = 'modal__teaxarea';
+		this.modalTextArea.classList.add('modal__teaxarea');
 		this.modalTextArea.id = 'taskModal__descriptionInput';
 		this.modalTextArea.placeholder = 'Add detailse...';
 		this.modalDescripton.append(this.modalTextArea);
 
-		this.modalFooter.classList = 'block modal__footer';
+		this.modalFooter.classList.add('modal__footer');
+		this.modalFooter.classList.add('block');
 		this.modalFooter.id = 'taskModal__footer';
 		this.modal.append(this.modalFooter);
 
-		this.modalCancelButton.classList = 'modal__button modal__button-cancel';
+		this.modalCancelButton.classList.add('modal__button-cancel');
+		this.modalCancelButton.classList.add('modal__button');
 		this.modalCancelButton.id = 'taskModal__cancelButton';
 		this.modalCancelButton.textContent = 'Cancel';
 		this.modalFooter.append(this.modalCancelButton);
 
-		this.modalSubmitButton.classList = 'modal__button modal__button-submit';
+		this.modalSubmitButton.classList.add('modal__button-submit');
+		this.modalSubmitButton.classList.add('modal__button');
 		this.modalSubmitButton.id = 'taskModal__submitButton';
 		this.modalSubmitButton.textContent = 'Submit';
 		this.modalFooter.append(this.modalSubmitButton);
@@ -160,13 +186,35 @@ export default class ViewModel {
 				this.modalInput.placeholder = 'the field must not be empty';
 				this.modalInput.classList.add('error');
 			}
-			//displayTasks();
+			this.displayTaskList();
 		});
 	}
 
-	/*displayTasks(){
+	displayTaskList(){
+		this.task.classList.add('createElement');
+		this.task.id = 'taskItem';
+		this.taskList.prepend(this.task);
 
-}*/
+		this.taskCheckBox.classList.add('task-checkbox');
+		this.taskCheckBox.id = 'checkbox';
+		this.task.prepend(this.taskCheckBox);
+
+		this.taskTittle.classList.add('title-task');
+		this.taskTittle.id = 'taskTitle';
+		this.taskCheckBox.prepend(this.taskTittle);
+
+		this.taskSubtitle.classList.add('textDescription-task');
+		this.taskSubtitle.id = 'taskDescription';
+		this.taskCheckBox.prepend(this.taskSubtitle);
+
+		this.taskEditButton.classList.add('edit-button');
+		this.taskEditButton.id = 'editButton';
+		this.taskSubtitle.prepend(this.taskEditButton);
+
+		this.taskDeleteButton.classList.add('delete-button');
+		this.taskDeleteButton.id = 'deleteButton';
+		this.taskEditButton.prepend(this.taskDeleteButton);
+	}
 
 	init() {
 	// Method which initialaize empty state UI
